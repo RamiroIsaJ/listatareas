98c84f9f8fcc4230e73d87e63b026e5cf0911c2c
RamiroIsaJ <ramfer.isaj@gmail.com>
2021-11-09 17:19:24 -0300

    agregado states y evento submit

--- src/components/FormTareas.js
+++ src/components/FormTareas.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Form, Button } from 'react-bootstrap';
 import ListaTareas from './ListaTareas';
 
 const FormTareas = () => {
+    // crear los states
+    const [tareaInd, setTareaInd] = useState('');
+    const [tareas, setTareas] = useState([]);
+    
+    // const altaTarea = (e) => {
+    //    console.log(e.target.value);
+        // guardar en un state
+    //    setTareaInd(e.target.value);
+    //}
+
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        // guardar en el arreglo de tareas
+        setTareas([...tareas, tareaInd]);  // operador spread para agregar un elemento al arreglo tareas
+        setTareaInd('');
+    }
+
     return (
         <div>
-            <Form className="container my-5">
+            <Form className="container my-5" onSubmit={handleSubmit}>
                 <Form.Group className="mb-3 d-flex">
-                    <Form.Control type="text" placeholder="Ingrese una tarea" />
+                    <Form.Control type="text" placeholder="Ingrese una tarea" onChange={e => setTareaInd(e.target.value)} value={tareaInd}/>
                     <Button variant="primary" type="submit"> Submit </Button>
                 </Form.Group>
             </Form>
--- src/components/Titulo.js
+++ src/components/Titulo.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Titulo extends Component {
     render() {
         return (
-            <h1 class="my-5 display-3 text-center text-light">Primer proyecto REACT</h1>
+            <h1 className="my-5 display-3 text-center text-light">Primer proyecto REACT</h1>
         );
     }
 }
